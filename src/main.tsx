4573ac273f9191a3d3777edd03663b37ee490c9c
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-20 23:41:10 -0300

    feat: add apollo provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css';
 
+import { ApolloProvider } from '@apollo/client';
+import { client } from 'lib/apollo';
+
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
