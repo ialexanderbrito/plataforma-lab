51d6c284469c9d2ad858b679b368d2eccbdb1f34
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-21 17:02:07 -0300

    fix: new uri cms

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o4nkv40hfa01xr7qr0a4rr/master',
   cache: new InMemoryCache(),
 });
