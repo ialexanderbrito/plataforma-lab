99af812b439199b558917a861387c44758774b7d
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-20 23:43:01 -0300

    feat: add tag li get lessons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,27 @@
+import { gql, useQuery } from '@apollo/client';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 export function Homepage() {
-  return <h1 className="text-5xl font-bold text-violet-500">Hello world</h1>;
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+
+  return (
+    <ul>
+      {data?.lessons?.map((lesson) => (
+        <li key={lesson.id}>{lesson.title}</li>
+      ))}
+    </ul>
+  );
 }
