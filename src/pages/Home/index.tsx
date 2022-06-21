b11d9cafdf69dd4cc6b39998c331c3791dd54b08
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-20 22:01:41 -0300

    feat: add text h1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,3 @@
-import { useEffect } from 'react';
-
-import { useTheme } from 'contexts/Theme';
-import { useToast } from 'contexts/Toast';
-
-import styles from './Home.module.scss';
-
 export function Homepage() {
-  const { theme } = useTheme();
-  const { toast } = useToast();
-
-  useEffect(() => {
-    toast.success('Bem vindo ao template alx');
-  }, [toast]);
-
-  return (
-    <>
-      <div className={styles.home} data-theme={theme}>
-        <h1>
-          alx<span>boilerplate</span>
-        </h1>
-      </div>
-    </>
-  );
+  return <h1 className="text-5xl font-bold text-violet-500">Hello world</h1>;
 }
