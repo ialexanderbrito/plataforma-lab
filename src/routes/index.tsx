a2ec937c4b71740a42d1259fd51bc156960afa17
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-21 21:51:20 -0300

    fix: import page event

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { Homepage } from 'pages/Home';
+import { Event } from 'pages/Event';
 
 export function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Event />} />
     </Routes>
   );
 }
