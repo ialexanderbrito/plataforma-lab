fcc716fec229229c836179d61cc3c50b37aaa39a
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-22 23:35:17 -0300

    fix: import apollo provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import { ApolloProvider } from '@apollo/client';
+import { client } from 'lib/apollo';
 import { MainRoutes } from 'routes';
 
 import { ToastProvider } from 'contexts/Toast';
 
 export function App() {
   return (
-    <ToastProvider>
-      <BrowserRouter>
-        <MainRoutes />
-      </BrowserRouter>
-    </ToastProvider>
+    <ApolloProvider client={client}>
+      <ToastProvider>
+        <BrowserRouter>
+          <MainRoutes />
+        </BrowserRouter>
+      </ToastProvider>
+    </ApolloProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css';
 
-import { ApolloProvider } from '@apollo/client';
-import { client } from 'lib/apollo';
-
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
