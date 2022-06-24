c2064b5acf7a394f122fd5d68592558e41e0aafb
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-24 20:37:04 -0300

    feat: add import card video

--- src/pages/Event/index.tsx
+++ src/pages/Event/index.tsx
@@ -1,16 +1,18 @@
 import { useParams } from 'react-router-dom';
 
+import { CardVideo } from 'components/CardVideo';
 import { Header } from 'components/Header';
 import { Sidebar } from 'components/Sidebar';
 import { Video } from 'components/Video';
 
 export function Event() {
   const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        {slug ? <Video lessonSlug={slug} /> : <div className="flex-1" />}
+        {slug ? <Video lessonSlug={slug} /> : <CardVideo />}
         <Sidebar />
       </main>
     </div>
